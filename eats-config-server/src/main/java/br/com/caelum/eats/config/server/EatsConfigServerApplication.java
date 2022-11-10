c5d060935a10c729262cb965f66b68d3a5bb1b1f
Ramiro <ramirojavadeveloper@gmail.com>
2022-11-10 10:00:25 -0300

    adicao de dependencia tomcat

--- eats-config-server/src/main/java/br/com/caelum/eats/config/server/EatsConfigServerApplication.java
+++ eats-config-server/src/main/java/br/com/caelum/eats/config/server/EatsConfigServerApplication.java
@@ -2,13 +2,14 @@ package br.com.caelum.eats.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication
 @EnableConfigServer
-public class EatsConfigServerApplication {
+public class EatsConfigServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EatsConfigServerApplication.class, args);
--- eats-service-registry/src/main/java/br/com/caelum/eats/registry/EatsServiceRegistryApplication.java
+++ eats-service-registry/src/main/java/br/com/caelum/eats/registry/EatsServiceRegistryApplication.java
@@ -2,11 +2,12 @@ package br.com.caelum.eats.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EatsServiceRegistryApplication {
+public class EatsServiceRegistryApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EatsServiceRegistryApplication.class, args);
