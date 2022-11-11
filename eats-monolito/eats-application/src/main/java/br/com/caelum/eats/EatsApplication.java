96fb8cfc145b19d5b82567fa436157be0a0a9fef
Ramiro <ramirojavadeveloper@gmail.com>
2022-11-11 09:40:46 -0300

    correção de pom xml

--- eats-monolito/eats-application/src/main/java/br/com/caelum/eats/EatsApplication.java
+++ eats-monolito/eats-application/src/main/java/br/com/caelum/eats/EatsApplication.java
@@ -2,11 +2,12 @@ package br.com.caelum.eats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class EatsApplication {
+public class EatsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EatsApplication.class, args); 
