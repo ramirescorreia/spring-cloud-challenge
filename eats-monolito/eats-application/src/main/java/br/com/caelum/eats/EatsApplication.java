88e035ce94017f90dd71bb055fba066317bfd221
Ramiro <ramirojavadeveloper@gmail.com>
2022-11-14 16:06:05 -0300

    mudando config logback xml para programatica

--- eats-monolito/eats-application/src/main/java/br/com/caelum/eats/EatsApplication.java
+++ eats-monolito/eats-application/src/main/java/br/com/caelum/eats/EatsApplication.java
@@ -1,16 +1,18 @@
 package br.com.caelum.eats;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import br.com.caelum.eats.configuration.LoggingIntializer;
 
 @EnableFeignClients
 @SpringBootApplication
 public class EatsApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
-		SpringApplication.run(EatsApplication.class, args); 
+		new SpringApplicationBuilder(EatsApplication.class).initializers(new LoggingIntializer()).run(args);
 	}
 
 }
--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
@@ -1,16 +1,19 @@
 package br.com.caelum.eats.pagamento;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import br.com.caelum.eats.pagamento.config.LoggingIntializer;
+
+
 
 @EnableFeignClients
 @SpringBootApplication
 public class EatsPagamentoServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EatsPagamentoServiceApplication.class, args);
+		new SpringApplicationBuilder(EatsPagamentoServiceApplication.class).initializers(new LoggingIntializer()).run(args);
 	}
 
 }
