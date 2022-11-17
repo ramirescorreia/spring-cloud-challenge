4fb03e26fc56e338c4b454c593356b42dbd0d9e0
pbarros1979 <pbarros1979@gmail.com>
2022-11-17 14:14:20 -0300

    Ajustas para docker compose

--- eats-service-registry/src/main/java/br/com/caelum/eats/registry/EatsServiceRegistryApplication.java
+++ eats-service-registry/src/main/java/br/com/caelum/eats/registry/EatsServiceRegistryApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
 @EnableEurekaServer
+
 public class EatsServiceRegistryApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
