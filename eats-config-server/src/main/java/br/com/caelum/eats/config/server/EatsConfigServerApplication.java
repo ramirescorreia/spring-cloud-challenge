afb799937088f032b2167fe21f2ff9345c139025
pbarros1979 <pbarros1979@gmail.com>
2022-11-19 14:12:36 -0300

    Ajustes docker compose, versão cloud e add TypeHint config

--- eats-config-server/src/main/java/br/com/caelum/eats/config/server/EatsConfigServerApplication.java
+++ eats-config-server/src/main/java/br/com/caelum/eats/config/server/EatsConfigServerApplication.java
@@ -5,10 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.nativex.hint.TypeHint;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInfo;
+import com.netflix.discovery.shared.Application;
 
 @Configuration
 @SpringBootApplication
 @EnableConfigServer
+@TypeHint(types = { Application.class, InstanceInfo.class, MyDataCenterInfo.class }, typeNames = {
+		"com.netflix.discovery.shared.Application",
+		"com.netflix.appinfo.InstanceInfo",
+		"com.netflix.appinfo.InstanceInfo$PortWrapper",
+		"com.netflix.appinfo.DataCenterInfo"
+})
 public class EatsConfigServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
