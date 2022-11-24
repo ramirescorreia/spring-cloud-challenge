1c9440e52dc5725a91de6cbd1f89427c14078a10
Ramiro <ramirojavadeveloper@gmail.com>
2022-11-24 16:37:47 -0300

    correçao projeto pagamento service

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
@@ -3,6 +3,11 @@ package br.com.caelum.eats.pagamento;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.nativex.hint.TypeHint;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInfo;
+import com.netflix.discovery.shared.Application;
 
 import br.com.caelum.eats.pagamento.config.LoggingIntializer;
 
@@ -10,6 +15,12 @@ import br.com.caelum.eats.pagamento.config.LoggingIntializer;
 
 @EnableFeignClients
 @SpringBootApplication
+@TypeHint(types = { Application.class, InstanceInfo.class, MyDataCenterInfo.class }, typeNames = {
+		"com.netflix.discove,ry.shared.Application",
+		"com.netflix.appinfo.InstanceInfo",
+		"com.netflix.appinfo.InstanceInfo$PortWrapper",
+		"com.netflix.appinfo.DataCenterInfo",
+})
 public class EatsPagamentoServiceApplication {
 
 	public static void main(String[] args) {
