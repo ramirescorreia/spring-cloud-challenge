1a1502167a9b5a6f71eb58fbfb8ceec7a178161f
Ramiro <ramirojavadeveloper@gmail.com>
2022-12-02 10:56:31 -0300

    correção de entidades

--- eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/Restaurante.java
+++ eats-distancia-service/src/main/java/br/com/caelum/eats/distancia/Restaurante.java
@@ -5,12 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "restaurante_distancia")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
