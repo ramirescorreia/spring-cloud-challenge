8f8e1beaf2aa94a9dc7d625aa43df29fcd25202a
Ramiro <ramirojavadeveloper@gmail.com>
2022-12-06 11:58:12 -0300

    correção ao salvar restaurante no banco

---
+++ eats-monolito/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/repository/TipoDeCozinhaRepository.java
@@ -0,0 +1,9 @@
+package br.com.caelum.eats.restaurante.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.caelum.eats.administrativo.TipoDeCozinha;
+
+public interface TipoDeCozinhaRepository extends JpaRepository<TipoDeCozinha, Long>{
+
+}
