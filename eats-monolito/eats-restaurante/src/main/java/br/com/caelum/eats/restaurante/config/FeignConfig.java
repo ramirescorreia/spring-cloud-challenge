f63fc7c81275054317d199b213510470722cffa3
Ramiro <ramirojavadeveloper@gmail.com>
2022-12-08 17:33:07 -0300

    adição de load balancer

--- eats-monolito/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/config/FeignConfig.java
+++ eats-monolito/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package br.com.caelum.eats.restaurante.config;
 
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,7 @@ import feign.Logger;
 
 
 @Configuration
+@LoadBalancerClient(value = "distRestService", configuration = CustomLoadBalancerConfiguration.class)
 public class FeignConfig {
 
 	@Bean
